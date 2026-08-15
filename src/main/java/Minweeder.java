3df2bb34a59244f931c32dd85b100a631eff4fe3
minweed <minegreenbasil@gmail.com>
2026-08-15 17:50:38 +0800

    feat: add greeting and exit

--- src/main/java/Minweeder.java
+++ src/main/java/Minweeder.java
@@ -5,6 +5,15 @@ public class Minweeder {
                 + "| |\\/| || ||  \\| |\\ \\ /\\ / /|  _| |  _| | | | |  _| | |_) |\n"
                 + "| |  | || || |\\  | \\ V  V / | |___| |___| |_| | |___|  _ < \n"
                 + "|_|  |_|___|_| \\_|  \\_/\\_/  |_____|_____|____/|_____|_| \\_\\\n";
+        String line = "────────────────────────────────────────────────────────────────\n";
+        String greeting = "Heyyo I'm Minweeder!\nLETS GET THINGS DONE RAHH\n";
+        String goodbye = "Goodbye! Hope you had a productive session :)\n";
+        System.out.print(line);
         System.out.println(banner);
+        System.out.print(greeting);
+        System.out.print(line);
+        System.out.print(goodbye);
+        System.out.print(line);
+
     }
 }
