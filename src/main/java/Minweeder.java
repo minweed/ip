0f0d4293fbac1f5be0f8dbb0306a9a1ffe4089b7
minweed <minegreenbasil@gmail.com>
2026-08-18 00:08:53 +0800

    feat: add list marking / unmarking

--- src/main/java/Minweeder.java
+++ src/main/java/Minweeder.java
@@ -16,12 +16,13 @@ public class Minweeder {
         System.out.print(greeting);
         System.out.print(line);
 
-        String[] storage = new String[100];
+        Task[] storage = new Task[100];
         int counter = 0;
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             String command = scanner.nextLine();
+            String[] breakdown = command.split(" ");
 
             if (command.equals("bye")) {
                 System.out.print(line);
@@ -30,13 +31,32 @@ public class Minweeder {
                 break;
             } else if (command.equals("list")) {
                 System.out.print(line);
+                System.out.println("Here are your tasks:");
                 for (int i = 0; i < counter; i++) {
                     System.out.println((i + 1) + ". " + storage[i]);
                 }
                 System.out.print(line);
-            } else {
+            } else if (breakdown[0].equals("mark")) {
+                int taskNumber = Integer.parseInt(breakdown[1]);
+                int index = taskNumber - 1;
+                storage[index].mark();
                 System.out.print(line);
-                storage[counter] = command;
+                System.out.println("Congrats! Task has been marked as completed:");
+                System.out.println("  " + storage[index]);
+                System.out.println(line);
+            } else if (breakdown[0].equals("unmark")) {
+                int taskNumber = Integer.parseInt(breakdown[1]);
+                int index = taskNumber - 1;
+                storage[index].unmark();
+                System.out.print(line);
+                System.out.println("Done! Task has been marked as not done yet:");
+                System.out.println(" " + storage[index]);
+                System.out.println(line);
+            }
+            else {
+                System.out.print(line);
+                Task task = new Task(command);
+                storage[counter] = task;
                 counter++;
                 System.out.println("added: " + command);
                 System.out.print(line);
